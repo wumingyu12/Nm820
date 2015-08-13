e8fd92264d8a7627c456097640ec39f270fe7b16
wumingyu <317808023@qq.com>
2015-08-13 17:23:11 +0800

    加入了angular chart图表的例子
    Signed-off-by: wumingyu <317808023@qq.com>

--- frontWeb/js/App.js
+++ frontWeb/js/App.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('MyApp', ['ngAnimate','MyApp.page1','MyApp.page2']);//[]里可以注入模块
+var myApp = angular.module('MyApp', ['ngAnimate','MyApp.page1','MyApp.page2','chart.js']);//[]里可以注入模块
 
 myApp.controller('bodyCtrl',[
 	'$scope',
@@ -24,6 +24,22 @@ myApp.controller('navleftCtrl',[
 		};
 	}
 ]);
+
+myApp.controller('LineCtrl',[
+	'$scope',
+	function ($scope){
+		$scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+		$scope.series = ['Series A', 'Series B'];
+		$scope.data = [
+			[65, 59, 80, 81, 56, 55, 40],
+	    	[28, 48, 40, 19, 86, 27, 90]
+	  	];
+		$scope.onClick = function (points, evt) {
+	    	console.log(points, evt);
+		};
+	}
+]);
+
 
 
 
